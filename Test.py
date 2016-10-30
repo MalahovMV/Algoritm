e305cf6b89b7788de940886c6394242591ffb03e
MalahovMV <misha.malaxow@yandex.ru>
2016-10-30 20:22:09 +0300

    New version

--- Test.py
+++ Test.py
@@ -127,6 +127,29 @@ def TestOut(nfaila):
             currentstars = copy.deepcopy(nextstars)
             nextstars = {}
 
+#Проверка правильности ответа к тестирующему входу
+def TestAns(nfaila):
+    fileOut = open('Output/vyhod' + nfaila + '.txt', 'r')
+    fileAns = open('ExpectAnsw/vyhod' + nfaila + '.txt', 'r')
+    #Сверяем полученный файл и ожидаемый
+    for sOut in fileOut:
+        sAns = fileAns.readline()
+        i = 0
+        for element in sOut:
+            flag = False
+            if not (element == sAns[i]):
+                flag = True
+
+            if flag:
+                raise Exception('Задача решена неверно')
+
+            i+=1
+
+    print ('Был получен верный результат')
+
+nfaila = input('Введите номер проверочного файла')
+TestAns(nfaila)
+
 if __name__ == '__main__':
     change = input('''Введите 0, если хотите проверить корректность входного файла,
                    введите 1, если хотите проверить корректность выходного файла''')
@@ -139,5 +162,6 @@ if __name__ == '__main__':
         nfaila = input('Введите номер  файла, который нужно проверить')
         TestIn(nfaila)
         print('Введенный файл корректен')
+	TestAns(nfaila)
 
 
